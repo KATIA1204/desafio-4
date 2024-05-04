96f9c8f04294807d5c7b2abd43eeff2f19cc20e9
KATIA1204 <142451981+KATIA1204@users.noreply.github.com>
2024-05-03 23:27:09 -0300

    first commit

---
+++ scrpt.js
@@ -0,0 +1,11 @@
+
+
+function aparece(ul){
+    const e= document.queryseclector(ul);
+        if(e.style.display === "none"){
+            e.style.display==="block";
+        }else{
+            e.style.display==="none";
+        }
+    }
+    
